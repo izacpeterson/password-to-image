423bc381bd0bdefcd3123cbe43bf90ef61466e1d
izacpeterson <izacpeterson@gmail.com>
2024-06-21 10:39:55 -0600

    first commit, password hash

---
+++ go.mod
@@ -0,0 +1,5 @@
+module password-to-image
+
+go 1.22.3
+
+require golang.org/x/crypto v0.24.0 // indirect
---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import "golang.org/x/crypto/bcrypt"
+
+func main() {}
+
+func HashPassword(password string) (string, error) {
+	hashedPW, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return "", err
+	}
+	return string(hashedPW), nil
+}
