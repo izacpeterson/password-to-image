ccdde9f726229bd9fa64207e1686dff60e9129cf
izacpeterson <izacpeterson@gmail.com>
2024-06-21 10:56:40 -0600

    encrypt and decrypt text

--- main.go
+++ main.go
@@ -1,13 +1,87 @@
 package main
 
-import "golang.org/x/crypto/bcrypt"
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"crypto/sha256"
+	"encoding/base64"
+	"fmt"
+	"io"
+)
 
-func main() {}
+func main() {
+	password := "mypassword123"
+	key := "myverystrongpasswordo32bitlength"
 
-func HashPassword(password string) (string, error) {
-	hashedPW, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	encrypted, err := encrypt(password, key)
+	if err != nil {
+		fmt.Println("ERROR: ", err)
+	}
+
+	fmt.Println("Encryped: ", encrypted)
+
+	decrypted, err := decrypt(encrypted, key)
+	if err != nil {
+		fmt.Println("ERROR: ", err)
+	}
+
+	fmt.Println("Decrypted: ", decrypted)
+
+}
+
+func deriveKey(password string) []byte {
+	hash := sha256.Sum256([]byte(password))
+	return hash[:]
+}
+
+func encrypt(plaintext, password string) (string, error) {
+	key := deriveKey(password)
+	plainTextBytes := []byte(plaintext)
+
+	block, err := aes.NewCipher(key)
 	if err != nil {
 		return "", err
 	}
-	return string(hashedPW), nil
+
+	aesGCM, err := cipher.NewGCM(block)
+	if err != nil {
+		return "", err
+	}
+
+	nonce := make([]byte, aesGCM.NonceSize())
+	if _, err = io.ReadFull(rand.Reader, nonce); err != nil {
+		return "", err
+	}
+
+	cipherText := aesGCM.Seal(nonce, nonce, plainTextBytes, nil)
+	return base64.StdEncoding.EncodeToString(cipherText), nil
+}
+
+func decrypt(ciphertext, password string) (string, error) {
+	key := deriveKey(password)
+	cipherTextBytes, err := base64.StdEncoding.DecodeString(ciphertext)
+	if err != nil {
+		return "", err
+	}
+
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return "", err
+	}
+
+	aesGCM, err := cipher.NewGCM(block)
+	if err != nil {
+		return "", err
+	}
+
+	nonceSize := aesGCM.NonceSize()
+	nonce, cipherText := cipherTextBytes[:nonceSize], cipherTextBytes[nonceSize:]
+
+	plainTextBytes, err := aesGCM.Open(nil, nonce, cipherText, nil)
+	if err != nil {
+		return "", err
+	}
+
+	return string(plainTextBytes), nil
 }
